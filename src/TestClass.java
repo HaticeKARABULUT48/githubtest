4ce96c948926aa53f6b6c3e6fe36a6b78d593811
HaticeKARABULUT48 <131279640+HaticeKARABULUT48@users.noreply.github.com>
2023-04-21 01:24:38 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,8 @@ public class TestClass {
     public static void main(String[] args) {
 
         System.out.println("merhaba github...");
+        System.out.println("githubtan merhaba ....");
+        
 
     }
 }
